7628252ec14cc067bd6c3d8ee0cdb96a2f8a0fa6
jonas.moapolela <jonas.moapolela@accenture.com>
2022-01-21 23:03:53 +0200

    init search component

---
+++ src/components/EmployeeSearch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmployeeSearch = () => {
+  return <>
+      <div className='search'>
+          <input placeholder='Search' />
+      </div>
+  </>;
+};
+
+export default EmployeeSearch;
