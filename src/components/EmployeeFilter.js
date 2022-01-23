e67cabc459ab8f75b2178d1aae5d7ad89d31d301
jonas.moapolela <jonas.moapolela@accenture.com>
2022-01-23 20:33:38 +0200

    added filter dropdown

--- src/components/EmployeeFilter.js
+++ src/components/EmployeeFilter.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const EmployeeFilter = (props) => {
   return <div className='filter'>
-      Filter <i class="fa fa-chevron-down"></i>
+     <i class="fa fa-filter"></i> Filter By <i class="fa fa-chevron-down"></i>
+     <ul>
+       <li>Age</li>
+       <li>Other</li>
+     </ul>
   </div>;
 };
 
